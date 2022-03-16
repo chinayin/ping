6367547bb4ee756cdf2dfc826a188d2fc0bacc07
lei.tian <whereismoney@qq.com>
2022-03-16 20:47:23 +0800

    fix alias

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import path from 'path'
 import legacy from '@vitejs/plugin-legacy'
 import pkg from './package.json'
 
+const r = p => path.resolve(__dirname, './src', p)
+
 export default ({ command }) => {
   return {
     server: { port: 3000 },
     preview: { port: 8080 },
     resolve: {
       alias: {
-        '/@': path.resolve(__dirname, './src')
+        '@': r('.')
       }
     },
     plugins: [
